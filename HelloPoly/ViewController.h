5704de590bf658825a59b51b8ddb05841bcb7852
dyzma <dyzma@pozmwcoe15525.war.roche.com>
2012-02-16 18:05:12 +0100

    Initial Commit

---
+++ HelloPoly/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloPoly
+//
+//  Created by dyzma on 2/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
